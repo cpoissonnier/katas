b2afe3f3973eda60309b745d43a39b74c25b128b
Clément Poissonnier <clement.poissonnier@ineat-conseil.fr>
2020-02-07 12:37:00 +0100

    chore(): ajout du kata trip service

---
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -0,0 +1,16 @@
+package org.craftedsw.tripservicekata.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTest {
+    @Test
+    void name() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+}
