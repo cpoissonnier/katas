04011e269db38510d395389b49395e313b06fbf3
Clément Poissonnier <clement.poissonnier@ineat-conseil.fr>
2020-02-07 12:35:31 +0100

    chore(): ajout d'un template de projet

---
+++ project-template/src/test/java/SomeClassTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SomeClassTest {
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
+}
